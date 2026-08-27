29d9d5f9df1a9654a2eca4a50f0c03bdb7b1ec45
lu20140621-png <lu20140621@gmail.com>
2026-08-27 01:50:09 -0700

    Prepare analytics config for Supabase

--- analytics-config.js
+++ analytics-config.js
@@ -1,7 +1,7 @@
 window.GSM_ANALYTICS_CONFIG = {
   enabled: false,
   site: "goal-scoring-moment",
-  collectEndpoint: "",
-  summaryEndpoint: "",
+  supabaseUrl: "",
+  supabaseAnonKey: "",
   heartbeatSeconds: 15
 };
